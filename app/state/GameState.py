39bf05eb8393d0512cc4a3b4326d6f0c73c2e1b7
JakubWujec <57007753+JakubWujec@users.noreply.github.com>
2023-12-03 08:50:58 +0100

    GameState key reorder

--- app/state/GameState.py
+++ app/state/GameState.py
@@ -5,13 +5,14 @@ from app.state.units import Unit
 class GameState:
     def __init__(self) -> None:
         self.worldSize = Vector2(16, 16)
+        self.ground = [[Vector2(2, 26)] * 16 for x in range(16)]
+        self.walls = [[None] * 16 for x in range(16)]
         self.units: [Unit] = [
             Unit(self, Vector2(8, 8), Vector2(13, 1)),
             Unit(self, Vector2(9, 9), Vector2(7, 26)),
             Unit(self, Vector2(10, 10), Vector2(7, 26)),
         ]
-        self.ground = [[Vector2(2, 26)] * 16 for x in range(16)]
-        self.walls = [[None] * 16 for x in range(16)]
+
         for x in range(1, 16, 2):
             for y in range(1, 16, 2):
                 self.walls[x][y] = Vector2(4, 26)
