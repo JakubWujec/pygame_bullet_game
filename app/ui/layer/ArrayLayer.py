bc5f0d780e80c5f3acb208a493faab0f79e123d3
JakubWujec <57007753+JakubWujec@users.noreply.github.com>
2023-12-08 21:43:42 +0100

    Not redrawing background

--- app/ui/layer/ArrayLayer.py
+++ app/ui/layer/ArrayLayer.py
@@ -1,16 +1,23 @@
+import pygame
 from pygame.math import Vector2
 from .Layer import Layer
 
 
 class ArrayLayer(Layer):
-    def __init__(self, ui, imageFile, gameState, array):
+    def __init__(self, ui, imageFile, gameState, array, surfaceFlags=pygame.SRCALPHA):
         super().__init__(ui, imageFile)
         self.gameState = gameState
         self.array = array
+        self.surface = None
+        self.surfaceFlags = surfaceFlags
 
     def render(self, surface):
-        for y in range(self.gameState.worldHeight):
-            for x in range(self.gameState.worldWidth):
-                tile = self.array[y][x]
-                if not tile is None:
-                    self.renderTile(surface, Vector2(x, y), tile)
+        # if didnt rendered yet
+        if self.surface is None:
+            self.surface = pygame.Surface(surface.get_size(), flags=self.surfaceFlags)
+            for y in range(self.gameState.worldHeight):
+                for x in range(self.gameState.worldWidth):
+                    tile = self.array[y][x]
+                    if not tile is None:
+                        self.renderTile(self.surface, Vector2(x, y), tile)
+        surface.blit(self.surface, (0, 0))
