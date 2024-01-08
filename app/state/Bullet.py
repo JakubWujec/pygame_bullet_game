9312e65c362ea7d746c50b74d43c1b571c4f2924
JakubWujec <57007753+JakubWujec@users.noreply.github.com>
2024-01-08 16:43:00 +0100

    nextStopPOsition refactor

--- app/state/Bullet.py
+++ app/state/Bullet.py
@@ -27,13 +27,19 @@ class Bullet(GameItem):
         return self.nextStopPosition().elementwise() - self.direction
 
     def nextStopPosition(self) -> Vector2:
+        moveVector = self.direction
         positionX, positionY = math.floor(self.position.x), math.floor(self.position.y)
-        directionX, directionY = self.direction.x, self.direction.y
 
-        if directionX > 0:
+        if moveVector.x > 0:
             positionX += 1.0
 
-        if directionY > 0:
+        if moveVector.x < 0 and positionX == self.position.x:
+            positionX -= 1.0
+
+        if moveVector.y > 0:
             positionY += 1.0
+
+        if moveVector.y < 0 and positionY == self.position.y:
+            positionY -= 1.0
 
         return Vector2(positionX, positionY)
