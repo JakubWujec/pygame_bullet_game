5add0a50673d87e09f72376bdb4bace9a2807bcb
JakubWujec <57007753+JakubWujec@users.noreply.github.com>
2024-01-08 17:00:56 +0100

    bullet.stopMoving function

--- app/commands/MoveBulletCommand.py
+++ app/commands/MoveBulletCommand.py
@@ -12,12 +12,9 @@ class MoveBulletCommand(Command):
         self.bullet = bullet
 
     def run(self):
-        # Should only stop at 0.0, 1.0 etc
-        # Compute new position
         direction = self.bullet.direction
         newPos = self.bullet.position + (self.state.bulletSpeed * direction)
         nextStopPosition = self.bullet.nextStopPosition()
-        currentStopPosition = self.bullet.currentStopPosition()
 
         # Don't allow another bullet position
         if newPos in map(lambda bullet: bullet.position, self.state.bullets):
@@ -28,29 +25,29 @@ class MoveBulletCommand(Command):
             if self.state.isWallAt(nextStopPosition) or self.state.isBrickAt(
                 nextStopPosition
             ):
-                self.bullet.direction = Vector2(0, 0)
-                self.bullet.position = currentStopPosition
+                self.stopMoving()
                 return
 
         # Dont't alow to stack bombs
         for otherBullet in self.state.bullets:
             if nextStopPosition == otherBullet.position and not otherBullet.isMoving():
-                self.bullet.direction = Vector2(0, 0)
-                self.bullet.position = currentStopPosition
+                self.stopMoving()
                 return
 
         # # Don't allow other unit positions
         for otherUnit in self.state.units:
             if newPos == otherUnit.position:
-                self.bullet.direction = Vector2(0, 0)
-                self.bullet.position = currentStopPosition
+                self.stopMoving()
                 return
 
         ## Don't allow enemy position
         for enemy in self.state.enemies:
             if newPos == enemy.position:
-                self.bullet.direction = Vector2(0, 0)
-                self.bullet.position = currentStopPosition
+                self.stopMoving()
                 return
 
         self.bullet.position = newPos
+
+    def stopMoving(self):
+        self.bullet.position = self.bullet.currentStopPosition()
+        self.bullet.direction = Vector2(0, 0)
