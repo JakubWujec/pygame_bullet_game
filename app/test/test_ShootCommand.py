631bbbbdc26b50052bede4ac046efb637ba19dfa
JakubWujec <57007753+JakubWujec@users.noreply.github.com>
2024-01-13 11:51:15 +0100

    Base test for shootCommand

---
+++ app/test/test_ShootCommand.py
@@ -0,0 +1,46 @@
+import unittest
+from unittest.mock import Mock
+from app.state import GameState, Unit, Orientation
+from app.commands import (
+    ShootCommand,
+)
+
+
+class TestShootCommand(unittest.TestCase):
+    def setUp(self):
+        self.mockState = Mock(spec=GameState)
+        self.mockUnit = Mock(spec=Unit)
+        self.shootCommand = ShootCommand(self.mockState, self.mockUnit)
+
+    def testCanUnitShootAlive(self):
+        self.mockUnit.status = "alive"
+        self.mockUnit.canShoot.return_value = True
+        self.assertTrue(self.shootCommand.canUnitShoot())
+
+    def testCanUnitShootDestroyed(self):
+        self.mockUnit.status = "destroyed"
+        self.mockUnit.canShoot.return_value = True
+        self.assertFalse(self.shootCommand.canUnitShoot())
+
+    def testCanUnitShootCannotShoot(self):
+        self.mockUnit.status = "alive"
+        self.mockUnit.canShoot.return_value = False
+        self.assertFalse(self.shootCommand.canUnitShoot())
+
+    def testCalculateBulletStartPosition(self):
+        # Mock the orientation
+        self.mockUnit.orientation = Orientation.RIGHT
+
+        # Mock the position
+        self.mockUnit.position = (1, 1)
+
+        # Call the method
+        result = self.shootCommand.calculateBulletStartPosition()
+
+        # Assert the result
+        expectedResult = (1.8, 1)
+        self.assertEqual(result, expectedResult)
+
+
+if __name__ == "__main__":
+    unittest.main()
