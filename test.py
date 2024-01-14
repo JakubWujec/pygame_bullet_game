0d8c7cea863737fd7cceaad0d88808db645ae4d5
JakubWujec <57007753+JakubWujec@users.noreply.github.com>
2024-01-14 11:15:24 +0100

    Change testLoader directory

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import unittest
 
 loader = unittest.TestLoader()
-tests = loader.discover("app")
+tests = loader.discover("./app")
 testRunner = unittest.runner.TextTestRunner()
 testRunner.run(tests)
