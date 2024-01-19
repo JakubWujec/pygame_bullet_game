18e26023f0389132f7a623b50fe510e61296a9c8
JakubWujec <57007753+JakubWujec@users.noreply.github.com>
2024-01-19 16:39:39 +0100

    Bullet layer type hint

--- app/layer/BulletsLayer.py
+++ app/layer/BulletsLayer.py
@@ -1,10 +1,13 @@
-from pygame.math import Vector2
+from typing import List, TYPE_CHECKING
 
 from .Layer import Layer
 
+if TYPE_CHECKING:
+    from app.state import Bullet
+
 
 class BulletsLayer(Layer):
-    def __init__(self, cellSize, imageFile, gameState, bullets):
+    def __init__(self, cellSize, imageFile, gameState, bullets: List["Bullet"]):
         super().__init__(cellSize, imageFile)
         self.gameState = gameState
         self.bullets = bullets
